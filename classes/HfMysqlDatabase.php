69be057e2dbca9a4b442a13ba39357064a307678
Nathan <narthur.a@gmail.com>
2015-06-04 10:14:39 -0500

    Made Db->GetAllReportsForGoal get db name properly

--- classes/HfMysqlDatabase.php
+++ classes/HfMysqlDatabase.php
@@ -613,7 +613,9 @@ class HfMysqlDatabase implements Hf_iDatabase {
     }
 
     public function getAllReportsForGoal($goalId, $userId) {
-        $format = 'SELECT * FROM `wp_hf_report` WHERE goalID = %d AND userID = %d';
+        $t = $this->cms->getDbPrefix() . 'hf_report';
+
+        $format = "SELECT * FROM $t WHERE goalID = %d AND userID = %d";
         $query = $this->cms->prepareQuery($format,array($goalId,$userId));
         return $this->cms->getResults($query);
     }
