631539de0f92662e1ad4a6dde4a45dcac71d73b7
Nathan Arthur <narthur@tutanota.com>
2017-05-28 17:33:01 -0400

    Isolate PhpLibrary tests

--- isolatedTests/classes/TestPhpLibrary.php
+++ isolatedTests/classes/TestPhpLibrary.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
-require_once( dirname( dirname( __FILE__ ) ) . '/HfTestCase.php' );
+require_once(dirname(dirname(__FILE__)) . '/HfTestCase.php');
 
 class TestPhpLibrary extends HfTestCase {
 
