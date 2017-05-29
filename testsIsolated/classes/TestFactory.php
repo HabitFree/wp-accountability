222fc3c2c2fa57fccd17cabc1a7162d8fb0394a0
Nathan Arthur <narthur@tutanota.com>
2017-05-29 10:58:30 -0400

    Refactor TestFactory

--- testsIsolated/classes/TestFactory.php
+++ testsIsolated/classes/TestFactory.php
@@ -3,91 +3,32 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 require_once(dirname(dirname(__FILE__)) . '/HfTestCase.php');
 
 class TestFactory extends HfTestCase {
-    // Helper Functions
-    
-    // Tests
+    public function testFactoryMakesObjects() {
+        $identifiers = [
+            "Goals" => "HfGoals",
+            "UserManager" => "HfUserManager",
+            "Messenger" => "HfMailer",
+            "AssetLocator" => "HfUrlFinder",
+            "MarkupGenerator" => "HfHtmlGenerator",
+            "Database" => "HfMysqlDatabase",
+            "CodeLibrary" => "HfPhpLibrary",
+            "Cms" => "HfWordPress",
+            "Security" => "HfSecurity",
+            "SettingsShortcode" => "HfSettingsShortcode",
+            "ManagePartnersShortcode" => "HfManagePartnersShortcode",
+            "DependencyChecker" => "HfDependencyChecker",
+            "GoalsShortcode" => "HfGoalsShortcode"
+        ];
 
-    public function testFactoryClassExists() {
-        $this->assertTrue( class_exists( 'HfFactory' ) );
-    }
-
-    public function testFactoryMakeGoals() {
-        $Goals = $this->factory->makeGoals();
-
-        $this->assertInstanceOf('HfGoals', $Goals);
-    }
-
-    public function testFactoryMakeUserManager() {
-        $UserManager = $this->factory->makeUserManager();
-
-        $this->assertInstanceOf('HfUserManager', $UserManager);
-    }
-
-    public function testFactoryMakeMailer() {
-        $Mailer = $this->factory->makeMessenger();
-
-        $this->assertInstanceOf('HfMailer', $Mailer);
-    }
-
-    public function testFactoryMakeUrlFinder() {
-        $UrlFinder = $this->factory->makeAssetLocator();
-
-        $this->assertInstanceOf('HfUrlFinder', $UrlFinder);
-    }
-
-    public function testFactoryMakeHtmlGenerator() {
-        $HtmlGenerator = $this->factory->makeMarkupGenerator();
-
-        $this->assertInstanceOf('HfHtmlGenerator', $HtmlGenerator);
-    }
-
-    public function testFactoryMakeDatabase() {
-        $Database = $this->factory->makeDatabase();
-
-        $this->assertInstanceOf('HfMysqlDatabase', $Database);
-    }
-
-    public function testFactoryMakePhpLibrary() {
-        $PhpLibrary = $this->factory->makeCodeLibrary();
-
-        $this->assertInstanceOf('HfPhpLibrary', $PhpLibrary);
-    }
-
-    public function testFactoryMakeWordPressInterface() {
-        $WordPressInterface = $this->factory->makeCms();
-
-        $this->assertInstanceOf('HfWordPress', $WordPressInterface);
-    }
-
-    public function testFactoryMakeSecurity() {
-        $Security = $this->factory->makeSecurity();
-
-        $this->assertInstanceOf('HfSecurity', $Security);
-    }
-
-    public function testFactoryMakeSettingsShortcode() {
-        $SettingsShortcode = $this->factory->makeSettingsShortcode();
-
-        $this->assertInstanceOf('HfSettingsShortcode', $SettingsShortcode);
-    }
-
-    public function testFactoryMakeManagePartnersShortcode() {
-        $ManagePartnersShortcode = $this->factory->makeManagePartnersShortcode();
-        $this->assertInstanceOf('HfManagePartnersShortcode', $ManagePartnersShortcode);
+        foreach ( $identifiers as $identifier => $class ) {
+            $method = "make$identifier";
+            $o = $this->factory->$method();
+            $this->assertInstanceOf( $class, $o );
+        }
     }
 
     public function testMakeLoginForm() {
         $f = $this->factory->makeLoginForm('jo');
         $this->assertInstanceOf('HfLoginForm', $f);
-    }
-
-    public function testMakeDependencyChecker() {
-        $d = $this->factory->makeDependencyChecker();
-        $this->assertInstanceOf('HfDependencyChecker', $d);
-    }
-
-    public function testMakeGoalsShortcode() {
-        $s = $this->factory->makeGoalsShortcode();
-        $this->assertInstanceOf("HfGoalsShortcode", $s);
     }
 }
