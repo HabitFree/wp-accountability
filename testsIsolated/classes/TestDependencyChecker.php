657cb0dbf6db61ea55be3d44df75fc720dffc536
Nathan Arthur <narthur@tutanota.com>
2017-05-29 10:05:28 -0400

    Fix Timber dependency

--- testsIsolated/classes/TestDependencyChecker.php
+++ testsIsolated/classes/TestDependencyChecker.php
@@ -4,13 +4,13 @@ require_once(dirname(dirname(__FILE__)) . '/HfTestCase.php');
 
 class TestDependencyChecker extends HfTestCase {
     public function testGetDependencyErrors() {
-        $this->expectAtLeastOnce( $this->mockCms, "isPluginActive", ["timber-library"] );
+        $this->expectAtLeastOnce( $this->mockCms, "isPluginActive", ["timber-library/timber.php"] );
         $this->mockedDependencyChecker->checkForDependencyErrors();
     }
 
     public function testRegistersError() {
         $this->expectAtLeastOnce( $this->mockCms, "addAdminErrorMessage", [
-            "hf-accountability requires the following plugins: timber-library"
+            "hf-accountability requires the following plugins: Timber"
         ] );
         $this->mockedDependencyChecker->checkForDependencyErrors();
     }
