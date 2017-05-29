d34eaba3a388213d7390b111768378ad2ac50335
Nathan Arthur <narthur@tutanota.com>
2017-05-29 11:07:54 -0400

    Configure Twig template location

--- classes/library/HfTimber.php
+++ classes/library/HfTimber.php
@@ -2,5 +2,10 @@
 
 class HfTimber
 {
-    public function render() {}
+    public function render( $template, $data ) {
+        Timber::$locations = dirname( __FILE__ ) . "/../../twig/";
+        $context = Timber::get_context();
+        $context = array_merge( $context, $data );
+        Timber::render($template, $context);
+    }
 }
